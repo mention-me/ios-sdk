ad0d801770116ffc0bf195f19aa2b8c681c814ef
Matt Long <matt@mention-me.com>
2019-09-26 11:29:02 +0100

    Ignore build

--- Example/build/Pods.build/Debug-iphoneos/MentionmeSwift.build/Objects-normal/arm64/MentionmeSwift_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/matt.long/Projects/FromMacBook/Projects/SDKiOS/Example/build/Pods.build/Debug-iphoneos/MentionmeSwift.build/DerivedSources/MentionmeSwift_vers.c
--- Example/build/Pods.build/Debug-iphonesimulator/MentionmeSwift.build/DerivedSources/MentionmeSwift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MentionmeSwiftVersionString[];
- extern const double MentionmeSwiftVersionNumber;
-
- const unsigned char MentionmeSwiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MentionmeSwift  PROJECT:Pods-1" "\n";
- const double MentionmeSwiftVersionNumber __attribute__ ((used)) = (double)1.;
