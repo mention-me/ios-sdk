be28843ed3e169178afd829e0409524cfb63a49e
Matt Long <matt@mention-me.com>
2023-01-04 16:42:29 +0000

    Remove segment from this request paramaeter class

--- MentionmeSwift/Classes/Requests/MentionmeRequest.swift
+++ MentionmeSwift/Classes/Requests/MentionmeRequest.swift
@@ -132,9 +132,6 @@ public class MentionmeRequest: NSObject {
         if let variation = mentionmeRequestParameters.variation{
             requestParams["variation"] = variation
         }
-        if let segment = mentionmeRequestParameters.segment{
-            requestParams["segment"] = segment
-        }
         
         return requestParams
     }
